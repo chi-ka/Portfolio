b3287c83750a4cfdef638bb0e4da072739dd76ee
chika ugonna okafor <chikaokafor@chikas-MacBook-Air.local>
2025-04-11 22:24:36 +0100

    changes on json bulid

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import React from 'react';
 import './styles/App.css';
 import ProjectPage from "./pages/ProjectPage";
