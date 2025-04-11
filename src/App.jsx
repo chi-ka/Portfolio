3cd96edf9cd67cd5060260ae63a8928a2285098c
chika <ochika6363@gmail.com>
2025-04-11 23:38:45 +0100

    Update deploy script to use dist/ folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import React from 'react';
-import './styles/App.css';
+import './styles/styles.css';
 import ProjectPage from "./pages/ProjectPage";
 import Home from "./pages/Home";
 import About from "./pages/About";
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,7 @@
 import { useState } from 'react'
-import '../styles/App.css'
-import Header from '../components/Header'
+import '../styles/styles.css'
 import Introduction from "../components/Introduction"
 import Skills from "../components/skills"
-import Projects from "../components/Projects"
 import Contact from "../components/contact"
 import Footer from "../components/footer"
 import Intro from "../components/intro"
